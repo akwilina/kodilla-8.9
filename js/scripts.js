03feefd18b91d320032e7e18d75832fd49bb5667
delola\delola lokal <aleksandra.man@gmail.com>
2017-03-02 21:47:18 +0100

    changed var and parameters names

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,15 @@
-function rysujZnaki(x, c) {
-    var gwiazdka = "";
-    for (var i = 1; i <= x; i++) {
-        gwiazdka = gwiazdka.concat(c);
+function drawChars(length, char) {
+    var element = "";
+    for (var i = 1; i <= length; i++) {
+        element = element.concat(char);
     }
-    return gwiazdka;
+    return element;
 }
 
-function rysujChoinke(y) {
+function drawChristmasTree(y) {
     for (var i = 1; i <= y; i++) {
-        console.log(rysujZnaki(y - i, " ") + rysujZnaki(i * 2 - 1, "*"))
+        console.log(drawChars(y - i, " ") + drawChars(i * 2 - 1, "*"))
     }
 }
 
-rysujChoinke(7);
+drawChristmasTree(7);
